51831c6ffeaac4b6498ff832519a811404b79323
Merik-D <myroslav.diakunchak@gmail.com>
2023-04-16 17:22:49 +0300

    making changes in the PlateManager tests

--- src/test/java/ua/lviv/iot/algo/part1/lab3/PlateManagerTest.java
+++ src/test/java/ua/lviv/iot/algo/part1/lab3/PlateManagerTest.java
@@ -1,33 +1,24 @@
 package ua.lviv.iot.algo.part1.lab3;
 
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-
-import java.util.LinkedList;
-import java.util.List;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class PlateManagerTest {
-    PlateManager plateManager;
-    Plate plate1;
-    Plate plate2;
-    Plate plate3;
-    Plate plate4;
+    private PlateManager plateManager;
 
-    @Before
+    @BeforeEach
     public void setUp() {
         plateManager = new PlateManager();
-        plate1 = new SaladPlate(24, "Porcelain", "Black", false, true, "rectangel", true);
-        plate2 = new SoupPlate(40, "Ceramic", "Gray", false, true, 15, "Mushroom broth");
-        plate3 = new PicnicPlate(15, "Faience", "Red", true, true, true, 5);
-        plate4 = new DessertPlate(18, "Porcelain", "Yellow", true, true, true, 3);
-        plateManager.addPlate(plate1);
-        plateManager.addPlate(plate2);
-        plateManager.addPlate(plate3);
-        plateManager.addPlate(plate4);
+        plateManager.addPlate(new SaladPlate(24, "Porcelain", "Black", false, true, "rectangel", true));
+        plateManager.addPlate(new SoupPlate(40, "Ceramic", "Gray", false, true, 15, "Mushroom broth"));
+        plateManager.addPlate(new PicnicPlate(15, "Faience", "Red", true, true, true, 5));
+        plateManager.addPlate(new DessertPlate(18, "Porcelain", "Yellow", true, true, true, 3));
     }
+
     @DisplayName("Test adding plates to the list")
     @Test
     public void testAddLaptop() {
@@ -37,20 +28,20 @@ public class PlateManagerTest {
     @DisplayName("Test finding plates with diameter greater than specified")
     @Test
     public void testFindPlateWithDiameterGreaterThan() {
-        List<Plate> expectedPlates = new LinkedList<>();
-        expectedPlates.add(new SaladPlate(24, "Porcelain", "Black", false, true, "rectangel", true));
-        expectedPlates.add(new SoupPlate(40, "Ceramic", "Gray", false, true, 15, "Mushroom broth"));
-        assertEquals(expectedPlates.toString(), plateManager.findPlateWithDiameterGreaterThan(20).toString());
+        var platesWithDiameterGreaterThan = plateManager.findPlateWithDiameterGreaterThan(20);
+        assertEquals(2, platesWithDiameterGreaterThan.size());
+        for (var plate : platesWithDiameterGreaterThan) {
+            assertTrue(plate.getDiameter() > 20);
+        }
     }
 
-    @DisplayName("Test finding all clean plates")
     @Test
-    public void testFindAllClean(){
-        List<Plate> expectedPlate = new LinkedList<>();
-        expectedPlate.add(new PicnicPlate(15, "Faience", "Red", true, true, true, 5));
-        expectedPlate.add(new DessertPlate(18, "Porcelain", "Yellow", true, true, true, 3));
-
-        //var platesAllClean = plateManager.findAllClean();
-        assertEquals(expectedPlate.toString(), plateManager.findAllClean().toString());
+    @DisplayName("Test finding all clean plates")
+    public void testFindAllClean() {
+        var cleanPlates = plateManager.findAllClean();
+        assertEquals(2, cleanPlates.size());
+        for (Plate plate : cleanPlates) {
+            assertTrue(plate.isClean());
+        }
     }
 }
